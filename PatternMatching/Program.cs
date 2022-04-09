c636647a8f03db5ed029699c7e3b5962cd3ac92b
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-09 05:45:05 +0200

    add pattern matching with switch case

--- PatternMatching/Program.cs
+++ PatternMatching/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using static System.Console;
 namespace PatternMatching
 {
     internal class Program
@@ -11,17 +11,39 @@ namespace PatternMatching
             //***************first example****************/
             int? notNullNumber = 10;
             if (notNullNumber is int x)
-                Console.WriteLine($"the value is number {x}");
+                WriteLine($"the value is number {x}");
             else
-                Console.WriteLine($"the value Not Found");
+                WriteLine($"the value Not Found");
             //***************second example****************/
 
             Person person = null;
             if (person is not null)
-                Console.WriteLine($"Has value is { person.Name}");
+                WriteLine($"Has value is { person.Name}");
             else if (person is null)
-                Console.WriteLine("Has No Person");
+                WriteLine("Has No Person");
 
+
+            PrintData(person);
+        }
+        private static void PrintData(object obj)
+        {
+            switch (obj)
+            {
+                case 0:
+                case 5:
+                case true:
+                    WriteLine($"you passed {obj}");
+                    break;
+                case int number:
+                    WriteLine($"you passed a numeric value");
+                    break;
+                case bool b:
+                    WriteLine($"you passed a boolean value");
+                    break;
+                default:
+                    WriteLine($"Invalid data");
+                    break;
+            }
         }
     }
 }
