5438344adcfa23313a9429f88e9bc6fdcbfb2001
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-04-09 05:17:10 +0200

    Add project files.

---
+++ PatternMatching/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace PatternMatching
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PatternMatching/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //***************first example****************/
+            int? notNullNumber = 10;
+            if (notNullNumber is int x)
+                Console.WriteLine($"the value is number {x}");
+            else
+                Console.WriteLine($"the value Not Found");
+            //***************second example****************/
+
+            Person person = null;
+            if (person is not null)
+                Console.WriteLine($"Has value is { person.Name}");
+            else if (person is null)
+                Console.WriteLine("Has No Person");
+
+        }
+    }
+}
